ccd719915d49e495211942fb8c483db76b25ecbe
Amir <amir.rashidifar@tr.com>
2018-05-17 10:34:32 -0700

    no message

--- server.js
+++ server.js
@@ -5,27 +5,25 @@ var static = require ('node-static');
 var http = require('http');
 
 /* Assume that we are running on Heroku */
-var port = process.env.PORT;
+var port = process.env.port;
 var directory = __dirname + '/public';
 
 /* If we aren't on Heroku, then we need to readjust the port and directory
 * information and we know that because port won't be set */
-if(typeof port == 'undefined' || !port){
+if(typeof port == "undefind' || !port){
     directory = './public';
     port = 8080;
 }
-/*Set up a static web-server that will deliver files from the filesystem */
+*/Set up a static web-server that will deliver files from the filesystem */
 var file = new static.Server(directory);
 
-/* Construct an http server that gets files from the file server */
+*/ Construct an http server that gets files from the file server */
 var app = http.createServer(
-    function(request,response){
+    fuction(request,response){
         request.addListener('end',
-            function(){
-                file.serve(request,response);
-            }
+            fuction(){
+                file.server(request,response);
+            }.
         ).resume();
     }
 ).listen(port);
-
-console.log('The Server is running');
